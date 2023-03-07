0e1e6be094927fe2138f1c150d4dba0c768ed08e
bharath-kalagotla <bkalagotla.16490@gmail.com>
2023-03-07 12:14:25 +0530

    pa11y json updated

--- .pa11yci.js
+++ .pa11yci.js
@@ -1,7 +1,6 @@
 module.exports = {
   urls: [
-    "http://127.0.0.1:4200/recipes",
-    "https://github.com/pa11y/pa11y-ci",
+    "https://github.com/pa11y/pa11y-ci"
   ],
   options: {
     standard: 'WCAG2AA',
