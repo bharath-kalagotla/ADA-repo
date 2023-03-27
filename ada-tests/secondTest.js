3c20584fec0dc4a28b929c6a556bab718f2ad65e
bharath-kalagotla <bkalagotla.16490@gmail.com>
2023-03-27 12:49:55 +0530

    ADA updates

---
+++ ada-tests/secondTest.js
@@ -0,0 +1,8 @@
+require('dotenv').config({
+  path: process.env.dotenv_config_path
+});
+
+module.exports = {
+  url: `${process.env.BASE_URL}/`,
+  actions: []
+}
---
+++ pa11y.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  launch_url: "http://127.0.0.1:4200",
+  src_folders: "ada-tests",
+  reports_path: "reports",
+  reporter: ["json", "html"],
+  test_settings: {
+    runners: [
+      'axe',
+      'htmlcs'
+    ],
+    standard: 'WCAG2A',
+    timeout: 120000,
+    includeNotices: true,
+    includeWarnings: true
+  },
+  puppeteer_settings: {
+    headless: false,
+    executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+  }
+};
